df3124e1677fb8b51ad02a971d4d8f3c02141521
seansgit <simon.small141@gmail.com>
2025-02-20 22:06:22 +0200

    l

--- students/views.py
+++ students/views.py
@@ -5,8 +5,9 @@ from .serializers import StudentSerializer,SampleSerializer
 from rest_framework import generics,permissions
 from django.contrib.auth.models import User
 from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
 
-
+@csrf_exempt
 class SamplesListCreate(generics.ListCreateAPIView):
     serializer_class =SampleSerializer
     permission_classes= [permissions.AllowAny]
@@ -30,6 +31,7 @@ class StudentsListCreate(generics.ListCreateAPIView):
         #return Todo.objects.filter(user=user,datecompleted__isnull=True )
         return student.objects.all()
     
+    
     def perform_create(self,serializer):
         #user = User.objects.get(pk=1)
         #serializer.save(user=self.request.user)
@@ -45,7 +47,7 @@ class StudentRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
         user = self.request.user
         return student.objects.all()
     
-
+@csrf_exempt
 class SampleRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     #serializer_class =TodoSerializer 
     #permission_classes= [permissions.IsAuthenticated]
