47dfcf83d26ab90eed19033bd0b55e963fec0f6c
seansgit <simon.small141@gmail.com>
2025-02-21 12:48:29 +0200

    d

--- students/urls.py
+++ students/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework import routers
-from .views import StudentsListCreate,StudentRetrieveUpdateDestroy,SamplesListCreate,SampleRetrieveUpdateDestroy
+from .views import SamplesListCreate,SampleRetrieveUpdateDestroy
 from . import views
 
 #router = routers.DefaultRouter()
@@ -9,7 +9,5 @@ from . import views
 #urlpatterns =router.urls
 urlpatterns = [
     path('samples/', SamplesListCreate.as_view()),
-    path('students/', StudentsListCreate.as_view()),
-    path('students/<int:pk>/',views.StudentRetrieveUpdateDestroy.as_view()),
     path('samples/<int:pk>/',views.SampleRetrieveUpdateDestroy.as_view()),
    ]
--- students/views.py
+++ students/views.py
@@ -7,7 +7,6 @@ from django.contrib.auth.models import User
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 
-@csrf_exempt
 class SamplesListCreate(generics.ListCreateAPIView):
     serializer_class =SampleSerializer
     permission_classes= [permissions.AllowAny]
@@ -22,32 +21,7 @@ class SamplesListCreate(generics.ListCreateAPIView):
         #serializer.save(user=self.request.user)
         serializer.save()
    
-class StudentsListCreate(generics.ListCreateAPIView):
-    serializer_class =StudentSerializer
-    permission_classes= [permissions.AllowAny]
 
-    def get_queryset(self):
-        user = self.request.user
-        #return Todo.objects.filter(user=user,datecompleted__isnull=True )
-        return student.objects.all()
-    
-    
-    def perform_create(self,serializer):
-        #user = User.objects.get(pk=1)
-        #serializer.save(user=self.request.user)
-        serializer.save()
-
-class StudentRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    #serializer_class =TodoSerializer 
-    #permission_classes= [permissions.IsAuthenticated]
-    serializer_class =StudentSerializer
-    permission_classes= [permissions.AllowAny]
-
-    def get_queryset(self):
-        user = self.request.user
-        return student.objects.all()
-    
-@csrf_exempt
 class SampleRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     #serializer_class =TodoSerializer 
     #permission_classes= [permissions.IsAuthenticated]
